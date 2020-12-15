6faee8aae609252f701b3541c8b50edfb4b49c15
andrew <fleming-andrew@protonmail.com>
2020-12-15 03:48:32 -0500

    updated comments in contracts

--- contracts/HodlFarm.sol
+++ contracts/HodlFarm.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.6.0;
 
 import './HodlToken.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/math/SafeMath.sol';
 
 interface DaiToken {
         function transfer(address dst, uint wad) external returns (bool);
@@ -11,10 +12,9 @@ interface DaiToken {
         }
 
 contract HodlFarm is Ownable {
+    using SafeMath for uint256;
 
-    event Staking(bool);
-
-    string public name = 'HodlFarm';
+    string public name = 'HodlFarm'; //for testing/smoke test
 
     HodlToken public hodlToken;
     DaiToken public daiToken;
@@ -26,54 +26,62 @@ contract HodlFarm is Ownable {
     mapping(address => uint256) public hodlBalance;
     mapping(address => bool) public isStaking;
 
+    /**@dev DaiToken address must be inserted in migrations when
+    ///     this contract deploys.
+     */
     constructor(HodlToken _hodlToken, DaiToken _daiToken) public {
         hodlToken = _hodlToken;
         daiToken = _daiToken;
     }
 
-    //staking
+    /**
+    *@notice A function that stakes stablecoin Dai to the contract.
+    *@dev After Dai transfers to the contract, the mapped staking balance updates. 
+    ///   This is necessary because the contract only pays out when the user withdraws 
+    ///   their earnings. The mapping keeps track of said yield.
+    *@param _amount The amount to be staked to the contract.
+     */
     function stake(uint256 _amount) public {
         require(_amount > 0, 'You cannot stake zero tokens');
-
-        //transfer after approved
         daiToken.transferFrom(msg.sender, address(this), _amount);
         stakingBalance[msg.sender] += _amount;
-
-        //update user status and mark beginning of yield earnings
         isStaking[msg.sender] = true;
         startTime[msg.sender] = block.timestamp;
-
-        emit Staking(true);
     }
 
-    //withdraw yield
+    /**@notice A method for withdrawing the hodlToken yield.
+    *@dev The timeStaked uint takes the result of the calculateYield function. 
+    ///   This contract gives the user 1% of their Dai balance in HodlToken every 60 
+    ///   seconds. After fetching the the calculated balance, the contract checks for 
+    ///   an existing balance mapped to hodlBalance. This mapping is only relevant if 
+    ///   the user staked Dai multiple times without unstaking/withdrawing. Further, the
+    ///   staking balance of the user is first multiplied by the time staked before
+    ///   divided by 100 to equate 1% of the user's stake (per minute as seen in the
+    ///   calculateYield function).
+     */
     function withdrawYield() public {
         uint timeStaked = calculateYield(msg.sender);
         uint bal = (stakingBalance[msg.sender] * timeStaked) / 100;
-
-        //check if leftover balance from prior staking remains
         if(hodlBalance[msg.sender] != 0){
-            //move balance to seperate variable
             uint oldBal = hodlBalance[msg.sender];
-            //remove mapping balance
             hodlBalance[msg.sender] = 0;
-            //add mapped balance with active balance
             bal += oldBal;
         }
-
-        //reset timestamp
         startTime[msg.sender] = block.timestamp;
-        
-        //transfer hodl
         hodlToken.transfer(msg.sender, bal);
     }
 
 
-    //for calculating and fetching
+    /**@notice A method for calculating yield.
+    *@dev The yield is calculated by first, subtracting the initial timestamp by the current 
+    ///   timestamp. Thereafter, dividing 60 (as in 60 seconds per minute) by the timestamp 
+    ///   difference. This function is left public so the frontend can fetch and display the 
+    ///   user's yield in real time.
+    *@param _usr The address that a user calls this function from/for.
+     */
     function calculateYield(address _usr) public view returns(uint){
         uint end = block.timestamp;
         uint totalTime = end - startTime[_usr];
-        //convert sec to minutes
         uint minTime = totalTime / 60;
         return minTime;
     }
@@ -104,8 +112,6 @@ contract HodlFarm is Ownable {
 
         //update staking status
         isStaking[msg.sender] = false;
-
-        emit Staking(false);
     }
 
 }
